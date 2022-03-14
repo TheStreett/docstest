9d455017db65164f2b25080a41cae01938e93f52
TheStreett <60371043+TheStreett@users.noreply.github.com>
2022-03-14 06:55:46 -0500

    update theme

--- source/conf.py
+++ source/conf.py
@@ -30,9 +30,10 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = [
+extensions = [
 #    'sphinx.ext.autodoc'
-#]
+    'karma_sphinx_theme'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
