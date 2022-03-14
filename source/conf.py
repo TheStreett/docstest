970398cd7ed0965e4b2c0e9b923148aa356a3e56
TheStreett <60371043+TheStreett@users.noreply.github.com>
2022-03-14 07:11:48 -0500

    update

--- source/conf.py
+++ source/conf.py
@@ -49,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import karma_sphinx_theme
+#import karma_sphinx_theme
 html_theme = 'karma_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
