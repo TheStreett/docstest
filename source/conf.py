a6dac22480f401a960dc0a4f7c5fe9b3686a4d7b
TheStreett <60371043+TheStreett@users.noreply.github.com>
2022-03-20 22:26:23 -0500

    add import

--- source/conf.py
+++ source/conf.py
@@ -30,6 +30,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import nbsphinx
 extensions = [
 #    'sphinx.ext.autodoc'
     "nbsphinx",
